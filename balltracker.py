b6182a3dd59ab0c1c8029a37854b57a65d539746
txh <txhqqpp@126.com>
2019-04-02 14:16:33 +0800

    dell color filter

--- balltracker.py
+++ balltracker.py
@@ -2,6 +2,7 @@ import cv2, random, os
 import numpy as np
 from sklearn.cluster import KMeans
 
+
 class ImgQueue:
   def __init__(self, maxsize=1):
     self.items = []
@@ -27,7 +28,8 @@ class ImgQueue:
     return len(self.items)
 
   def fgdifferential(self):
-    diff = np.abs(self.items[self.mid].astype(np.int) - self.items[self.mid + self.base[0]].astype(np.int)).astype(np.uint8)
+    diff = np.abs(self.items[self.mid].astype(np.int) - self.items[self.mid + self.base[0]].astype(np.int)).astype(
+      np.uint8)
     diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     _, diff = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)
     foreground = np.copy(diff)
@@ -39,91 +41,43 @@ class ImgQueue:
     return foreground
 
 
-que = ImgQueue(11)
-kernel_dilation1 = cv2.getStructuringElement(cv2.MORPH_RECT, (70, 70))
-kernel_dilation2 = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
-kernel_close = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-center = np.array([640, 360])
+def findball(que, n, kernel, k):
+  img = np.copy(que.items[que.mid])
+  img_diff = que.fgdifferential()
+  img_diff = cv2.morphologyEx(img_diff, cv2.MORPH_CLOSE, kernel)
+  circles = cv2.HoughCircles(img_diff, cv2.HOUGH_GRADIENT, 1, 10, param1=1, param2=2, minRadius=1, maxRadius=5)
+  if circles is None:
+    # cv2.imshow('no circle origin %d' % k, img)
+    print(k)
+    return None
+  circles = circles[0, :, :-1].astype(np.uint)
+  candidates = [[] for q in range(n)]
+  if len(circles) < n:
+    for c in circles:
+      cv2.circle(img, (c[0], c[1]), 3, (0,0,255), -1)
+    cv2.imshow('less circle %d' % k, img)
+    return None
+  kmeans.fit(circles)
+  for p in range(len(kmeans.labels_)):
+    candidates[kmeans.labels_[p]].append(circles[p])
+  return candidates
 
-
-def filter(clusters):
-  lt = []
-  t = []
-  for i in clusters:
-    mid = i[1] + i[3] // 2
-    if 80 <= mid < 180 or 370 <= mid < 520:
-      lt.append(i)
-    else:
-      t.append(i)
-  if len(lt) < 2:
-    return None, None
-  b2l = sorted(lt, key=lambda x: x[4], reverse=True)
-  players = b2l[:2]
-  ball = []
-  for i in range(2, len(b2l)):
-    t.append(b2l[i])
-  for i in t:
-    if players[1][1] + players[1][3] // 2 < i[1] + i[3] // 2 < players[0][1] + players[0][3] // 2:
-      if 200 < i[0] + i[2] // 2 < 1000:
-        ball.append(i)
-  # if len(ball) == 0:
-  #   ball = [None]
-  # else:
-  #   ball = sorted(ball, key=lambda x: np.linalg.norm(x[:2] + x[2:4] / 2 - center))
-  return players, ball
 
 if __name__ == '__main__':
+  que = ImgQueue(11)
   n = 8
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
   colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for i in range(n)]
   kmeans = KMeans(n_clusters=n, random_state=0)
   frames = os.listdir('img')
-  for f in range(50, len(frames)):
+  k = 5
+  for f in range(0, min(50000, len(frames))):
     img = cv2.imread('img/%s' % frames[f])
     que.enqueue(img)
     if que.isFull():
-      img_backup = np.copy(que.items[que.mid])
-      img_diff = que.fgdifferential()
-      cv2.imshow('debug0', img_diff)
-      # img_dilation = cv2.morphologyEx(img_diff, cv2.MORPH_CLOSE, kernel_close)
-      # cv2.imshow('debug1', img_dilation)
-      # nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(img_dilation)
-      # clusters_dilation = stats[1:, :]
-      # players, balls = filter(clusters_dilation)
-      # if players == None:
-      #   continue
-      # for player in players:
-      #   # if cluster[4] > 6500:
-      #   cv2.rectangle(img_backup, (player[0], player[1]), (player[0] + player[2], player[1] + player[3]), (0, 0, 255))
-      # # if ball is not None:
-      # for ball in balls:
-      #   # if cluster[4] > 6500:
-      #   cv2.rectangle(img_backup, (ball[0], ball[1]), (ball[0] + ball[2], ball[1] + ball[3]), (0, 0, 255))
-      # cv2.imshow('a', img_backup)
-      # if cluster[4] < 1000:
-      #   img_dilation[cluster[1]:cluster[1] + cluster[3], cluster[0]:cluster[0] + cluster[2]] = 0
-      # img_diff[img_dilation == 255] = 0
-      # cv2.imshow('debug3', img_diff)
-      circles = cv2.HoughCircles(img_diff, cv2.HOUGH_GRADIENT, 1, 10, param1=1, param2=2, minRadius=1, maxRadius=5)
-      if circles is not None:
-        points = []
-        result = [[] for q in range(n)]
-        k = 0
-        circles = circles[0, :, :-1].astype(np.int)
-        if len(circles) < n:
-          continue
-        kmeans.fit(circles)
-        lables = kmeans.labels_
-        for p in range(len(lables)):
-          result[lables[p]].append(circles[p])
-        for i in result:
-          if len(i) == 1:
-            for c in i:
-              if 200 < c[0] < 850 and c[1] < 550:
-                cv2.circle(img_backup, (c[0], c[1]), 3, (0,0,255), -1)
-          k += 1
-      cv2.imshow('debug', img_backup)
+      findball(que, n, kernel, k)
+    k += 1
     # terminate
     if cv2.waitKey(50) == 27:
       break
-  cap.release()
   cv2.destroyAllWindows()
